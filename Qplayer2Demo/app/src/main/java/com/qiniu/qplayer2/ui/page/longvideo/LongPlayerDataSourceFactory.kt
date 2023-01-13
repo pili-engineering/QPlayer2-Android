bb706f9609691ca5d0765a49c4d3419d0524016b
老干部 <chenjunqi@qiniu.com>
2023-01-13 16:49:43 +0800

    SEI点播测试视频更新

--- Qplayer2Demo/app/src/main/java/com/qiniu/qplayer2/ui/page/longvideo/LongPlayerDataSourceFactory.kt
+++ Qplayer2Demo/app/src/main/java/com/qiniu/qplayer2/ui/page/longvideo/LongPlayerDataSourceFactory.kt
@@ -567,7 +567,7 @@ object LongPlayerDataSourceFactory {
         )
 
         builder = QMediaModelBuilder()
-        url = "http://pili-playback.qnsdk.com/recordings/z1.sdk-live.6666/1652355051_1652355244.m3u8"
+        url = "http://pili-playback.qnsdk.com/recordings/z1.sdk-live.Hera/1673340696_1673340783.m3u8"
         builder.addElement("", QURLType.QAUDIO_AND_VIDEO, 1080, url, true)
         name = "26-点播-http-m3u8-SEI"
         videoParams = LongVideoParams(name, name.hashCode().toLong())
