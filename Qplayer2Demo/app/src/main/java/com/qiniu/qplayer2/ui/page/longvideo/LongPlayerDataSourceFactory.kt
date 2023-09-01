8756f0e6e69930edcae9cdd6fb52a33316521c45
老干部 <chenjunqi@qiniu.com>
2023-09-01 14:11:03 +0800

    更新测试视频

--- Qplayer2Demo/app/src/main/java/com/qiniu/qplayer2/ui/page/longvideo/LongPlayerDataSourceFactory.kt
+++ Qplayer2Demo/app/src/main/java/com/qiniu/qplayer2/ui/page/longvideo/LongPlayerDataSourceFactory.kt
@@ -1237,30 +1237,30 @@ object LongPlayerDataSourceFactory {
             )
         )
 
-        builder = QMediaModelBuilder()
-        builder.addStreamElement(
-            "", QURLType.QAUDIO_AND_VIDEO, 720,
-            "https://sdk-release.qnsdk.com/CENC_AES-CTR.mp4", true,
-            "",
-            "", QVideoRenderType.PLANE,
-            ""
-//            ,"c7e16c4403654b85847037383f0c2db3"
-        )
-        name = "46-mp4-drm"
-
-        videoParams = LongVideoParams(name, name.hashCode().toLong())
-        dataSourceBuilder.addVideo(
-            videoParams,
-            arrayListOf<LongPlayableParams>(
-                LongPlayableParams(
-                    builder.build(false),
-                    LongControlPanelType.Normal.type,
-                    DisplayOrientation.LANDSCAPE,
-                    LongEnviromentType.LONG.type,
-                    0
-                )
-            )
-        )
+//        builder = QMediaModelBuilder()
+//        builder.addStreamElement(
+//            "", QURLType.QAUDIO_AND_VIDEO, 720,
+//            "https://sdk-release.qnsdk.com/CENC_AES-CTR.mp4", true,
+//            "",
+//            "", QVideoRenderType.PLANE,
+//            ""
+////            ,"c7e16c4403654b85847037383f0c2db3"
+//        )
+//        name = "46-mp4-drm"
+//
+//        videoParams = LongVideoParams(name, name.hashCode().toLong())
+//        dataSourceBuilder.addVideo(
+//            videoParams,
+//            arrayListOf<LongPlayableParams>(
+//                LongPlayableParams(
+//                    builder.build(false),
+//                    LongControlPanelType.Normal.type,
+//                    DisplayOrientation.LANDSCAPE,
+//                    LongEnviromentType.LONG.type,
+//                    0
+//                )
+//            )
+//        )
 
         builder = QMediaModelBuilder()
         url = "https://video.sureemed.com/hls-mp4%2F20230717%E7%9F%AD%E8%A7%86%E9%A2%91hls-2023%E5%85%AD%E6%9C%88%E7%9F%AD%E8%A7%86%E9%A2%916.27.mp4-2000k-transcode.m3u8"
